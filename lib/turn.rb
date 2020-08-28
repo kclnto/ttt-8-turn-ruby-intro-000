656cf151d2e9d85322c686494619f9e5a1bc1999
Kacy Trujillo <kclnto@gmail.com>
2020-08-28 16:53:38 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -52,6 +52,7 @@ end
 
 def turn(board, index, player = "X") 
   input = gets.strip
+  input_to_index(input)
   move(board, index, player = "X")
   while (position_taken?(board,index) == TRUE) || (valid_move?(board,index) == FALSE)
     turn(board, index, player = "X")
